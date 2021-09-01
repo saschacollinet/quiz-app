41a8e3b275e57969dfb06c9f919b30188670c6df
Sascha Collinet <collinet.sascha@gmail.com>
2021-09-01 18:35:37 +0200

    Add toggle for show answer buttons

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+// navigation
+
 const mainHome = document.querySelector('#main__home')
 const mainBookmarks = document.querySelector('#main__bookmarks')
 const mainCreate = document.querySelector('#main__create')
@@ -54,4 +56,31 @@ buttonProfile.addEventListener('click', () => {
   buttonBookmarks.classList.remove('active')
   buttonCreate.classList.remove('active')
   buttonProfile.classList.add('active')
+})
+
+// show answer button one
+
+const buttonAnswerOne = document.querySelector('#card__buttonone')
+const AnswerOne = document.querySelector('#card__answerone')
+
+buttonAnswerOne.addEventListener('click', () => {
+  AnswerOne.classList.toggle('card__answer--show')
+})
+
+// show answer button two
+
+const buttonAnswerTwo = document.querySelector('#card__buttontwo')
+const AnswerTwo = document.querySelector('#card__answertwo')
+
+buttonAnswerTwo.addEventListener('click', () => {
+  AnswerTwo.classList.toggle('card__answer--show')
+})
+
+// show answer button three
+
+const buttonAnswerThree = document.querySelector('#card__buttonthree')
+const AnswerThree = document.querySelector('#card__answerthree')
+
+buttonAnswerThree.addEventListener('click', () => {
+  AnswerThree.classList.toggle('card__answer--show')
 })
